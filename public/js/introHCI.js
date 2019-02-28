ea4dd8a51caa0eeacefe7964e4dba56996b42bf1
Tamuz <Tamuzhod@gmail.com>
2019-02-28 10:33:08 -0800

    Added all the things

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,18 +10,36 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
+ 	initCamera();
+ 	initMap();
  	initGestures();
  	initRSVPForm();
 }
 
 // init jQuery gestures  
 function initGestures() {
-	// add gestures listener here
+	$(function(){
+		$(".judge-img").bind("taphold", tapholdHandler );
+		function tapholdHandler( event ){
+			var targetIDPrefix = event.target.id;
+			console.log("git prefix: " + targetIDPrefix);
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	})
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+	$("#rsvpForm").submit(function(e) {
+		e.preventDefault();
+		console.log("submiting form...");
+		var rsvpEmail = $("#rsvpEmail").val();
+
+		$.post('addRSVP', {rsvpEmail: rsvpEmail}, postCallback);
+	});
+
+	function postCallback(res) {
+		alert("RSVP form submited");
+		$('#rsvpEmail').val('');
+	}
 }
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = "gajfGAqffmARJmZFAapAb4qWv6nqyctN";
+
+	var map = L.mapquest.map('map', {
+		center: [32.8784341,-117.2358094],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.8784341,-117.2358094]).addTo(map);
 }
